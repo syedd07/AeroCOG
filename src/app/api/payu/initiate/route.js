38f69b9dab091ed68398d62b496a0394a5db7034
Syed Ali <syedyusufali6626@gmail.com>
2024-12-01 15:52:39 +0530

    mixor fixes on route.js

--- src/app/api/payu/initiate/route.js
+++ src/app/api/payu/initiate/route.js
@@ -33,9 +33,11 @@ export const POST = async (req) => {
       firstname,         // Customer's First Name
       email,             // Customer's Email
       phone,             // Customer's Phone (optional)
-      surl: "https:aerocog.tech/success", // Success URL
-      furl: "https:aerocog.tech/failure", // Failure URL
-      curl: "https:aerocog.tech/failure", // Cancel URL
+      service_provider: "payu_paisa", // Service Provider
+      hash: generatedHash, // Precomputed hash
+      surl: "https://aerocog.tech/success", // Success URL
+      furl: "https://aerocog.tech/failure", // Failure URL
+      curl: "https://aerocog.tech/failure", // Cancel URL
     };
 
     // Construct the string for hash generation
