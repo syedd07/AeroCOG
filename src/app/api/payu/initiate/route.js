5e066021f538ff2ce43ed11b7a9a776b27117d36
Syed Ali <syedyusufali6626@gmail.com>
2024-12-01 15:56:43 +0530

    new route change attempt-1

--- src/app/api/payu/initiate/route.js
+++ src/app/api/payu/initiate/route.js
@@ -34,7 +34,6 @@ export const POST = async (req) => {
       email,             // Customer's Email
       phone,             // Customer's Phone (optional)
       service_provider: "payu_paisa", // Service Provider
-      hash: generatedHash, // Precomputed hash
       surl: "https://aerocog.tech/success", // Success URL
       furl: "https://aerocog.tech/failure", // Failure URL
       curl: "https://aerocog.tech/failure", // Cancel URL
